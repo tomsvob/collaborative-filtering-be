479969ce6c4ad3b5874fac55ba90caf3b1ecdaa0
Tom Svoboda <tom.svoboda1@gmail.com>
2018-04-17 13:34:02 +0200

    data, db

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,15 +1,15 @@
 package app;
 
+import app.DAO.UserDAO;
+import app.Enums.UserType;
+import app.Models.User;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class Application {
@@ -19,17 +19,9 @@ public class Application {
     }
 
     @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
-
+    public CommandLineRunner loadData(UserDAO userDAO) {
+        return (args) -> {
+//            userDAO.save(new User(1, UserType.USER, ))
         };
     }
 
